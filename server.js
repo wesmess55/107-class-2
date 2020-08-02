a6c2adf06a58c8c73ecb683416f67f4e9690bf84
wesmess55 <66344929+wesmess55@users.noreply.github.com>
2020-08-02 13:00:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ server.get('/',function(req, res){
 server.get('/about',function(req,res){
     res.send("<h1 style='color:blue;'>Wes Ray<h1>");
 });
-server.listen(8080, function(){
-    console.log("Server running at http://localhost:8080");
-});
 
 server.get('/soon',function(req,res){
     res.send("<h1 style= 'weight:bold;color:grey;'>Page Coming Soon<h1>");
+});
+
+server.get('/contact',function(req,res){
+    res.send("Page under construction...");
+});
+
+server.post('api/item', function(req,res){
+
+});
+server.listen(8080, function(){
+    console.log("Server running at http://localhost:8080");
 });
